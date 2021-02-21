dc42c55cfd7e4acbda39f1b3aadc8a8c94bd86f7
mdgriffith <mdgriffith@users.noreply.github.com>
2021-02-21 16:43:48 -0500

    encoders and decoders for fwded msgs

--- ext-watchtower/Watchtower/Details.hs
+++ ext-watchtower/Watchtower/Details.hs
@@ -3,7 +3,11 @@
 module Watchtower.Details where
 
 import qualified Reporting.Annotation as Ann
-
+import qualified Json.Decode
+import qualified Json.String
+import qualified Json.Encode
+import Json.Encode ((==>))
+import Control.Applicative ((<|>), (<$>), (<*>))
 
 data Status
     = Success
@@ -12,11 +16,118 @@ data Status
 
 data Location =
     Location 
-        { _path :: FilePath
-        , _range :: Ann.Position
+        { _file :: FilePath
+        , _position :: Ann.Position
         }
 
 data Position = Position
 data Range = Range
 data Callgraph = CallGraph
 data Error = Error
+
+
+data Visible =
+    Visible 
+        { _active :: Maybe Editor
+        , _visible :: [ Editor ]
+        }
+
+
+data Editor =
+    Editor
+        { _path :: FilePath
+        , _visibleRanges :: [ Ann.Region ]
+        , _selection :: [ Ann.Region ]
+        }    
+
+
+-- ENCODERS
+
+encodeVisible :: Visible -> Json.Encode.Value
+encodeVisible (Visible active vis) =
+    Json.Encode.object 
+        [ ("active" ==>
+                (case active of
+                    Nothing ->
+                        Json.Encode.null
+                    Just edit ->
+                        encodeEditor edit
+                )
+            )
+        , ("visible" ==> Json.Encode.list encodeEditor vis)
+        ]
+
+encodeEditor :: Editor -> Json.Encode.Value
+encodeEditor (Editor path vis sel) =
+    Json.Encode.object 
+        [ ("path" ==> Json.Encode.string (Json.String.fromChars path))
+        , ("visibleRegions" ==> Json.Encode.list encodeRegion vis)
+        , ("selections" ==> Json.Encode.list encodeRegion sel)
+        ]
+
+encodeRegion :: Ann.Region -> Json.Encode.Value
+encodeRegion (Ann.Region start end) =
+    Json.Encode.object 
+        [ ("start" ==> encodePosition start)
+        , ("end" ==> encodePosition end)
+        ]  
+
+
+encodeLocation :: Location -> Json.Encode.Value
+encodeLocation (Location file pos) =
+    Json.Encode.object 
+        [ ("file" ==> Json.Encode.string (Json.String.fromChars file))
+        , ("pos" ==> encodePosition pos)
+        ]
+
+encodePosition :: Ann.Position -> Json.Encode.Value
+encodePosition (Ann.Position row col) =
+    Json.Encode.object 
+        [ ("row" ==> Json.Encode.int (fromIntegral row))
+        , ("col" ==> Json.Encode.int (fromIntegral col))
+        ]
+
+
+-- DECODERS
+
+decodeVisible :: Json.Decode.Decoder x Visible
+decodeVisible =
+    Visible
+        <$> (Json.Decode.field "active" 
+                (Json.Decode.oneOf 
+                    [ Just <$> decodeEditor
+                    , pure (Nothing)
+                    ]
+                )
+            )
+        <*> (Json.Decode.field "visible" 
+                (Json.Decode.list decodeEditor)
+            )
+
+decodeEditor :: Json.Decode.Decoder x Editor
+decodeEditor =
+    Editor 
+        <$> (Json.Decode.field "path" (Json.String.toChars <$> Json.Decode.string))
+        <*> (Json.Decode.field "visibleRegions" (Json.Decode.list decodeRegion))
+        <*> (Json.Decode.field "selections" (Json.Decode.list decodeRegion))
+
+
+decodeLocation :: Json.Decode.Decoder x Location
+decodeLocation =
+    Location 
+        <$> (Json.Decode.field "file" (Json.String.toChars <$> Json.Decode.string))
+        <*> (Json.Decode.field "pos" decodePosition)
+
+
+decodeRegion :: Json.Decode.Decoder x Ann.Region
+decodeRegion =
+    Ann.Region
+        <$> (Json.Decode.field "start" decodePosition)
+        <*> (Json.Decode.field "end" decodePosition)
+
+
+decodePosition :: Json.Decode.Decoder x Ann.Position
+decodePosition =
+    Ann.Position
+        <$> (Json.Decode.field "row" (fromIntegral <$> Json.Decode.int))
+        <*> (Json.Decode.field "col" (fromIntegral <$> Json.Decode.int))
--- ext-watchtower/Watchtower/Live.hs
+++ ext-watchtower/Watchtower/Live.hs
@@ -14,8 +14,10 @@ import Control.Concurrent.STM
 
 import qualified Data.ByteString.Builder
 import qualified Data.ByteString.Lazy
-
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+
+
 import qualified Network.WebSockets      as WS
 import qualified Network.WebSockets.Snap as WS
 
@@ -23,6 +25,7 @@ import qualified Develop.Generate.Help
 import qualified Json.Encode
 import qualified Json.String
 import Json.Encode ((==>))
+import qualified Json.Decode
 import qualified Reporting.Annotation as Ann
 import qualified Watchtower.StaticAssets
 import qualified Watchtower.Details
@@ -30,7 +33,7 @@ import qualified Ext.Sentry
 
 import qualified Watchtower.Websocket
 import qualified Watchtower.Compile
-import qualified Data.Text.Encoding as T
+
 import Ext.Common
 
 
@@ -71,30 +74,96 @@ websocket_ (State cache mClients) = do
           debug (T.unpack ("Total clients " <> T.pack (show totalClients)))
           pure Nothing
 
-        onReceive clientId text = do
+        -- onReceive clientId text = do
           
-          let (root, path) = T.span (\c -> c /= ':') text
+        --   let (root, path) = T.span (\c -> c /= ':') text
 
-          debug $ "Received " <> (T.unpack root) <> "  @ " <> (T.unpack (T.drop 1 path))
+        --   debug $ "Received " <> (T.unpack root) <> "  @ " <> (T.unpack (T.drop 1 path))
           
-          eitherStatusJson <- Watchtower.Compile.compileToJson ((T.unpack root) <> "/") (T.unpack (T.drop 1 path))
-          case eitherStatusJson of
-            Left errors -> do
-              debug $ "got errors"-- <> (T.unpack $ T.decodeUtf8 errors)
-              Watchtower.Websocket.broadcastImpl mClients (builderToString (encodeOutgoing (ElmStatus errors)))
+        --   eitherStatusJson <- Watchtower.Compile.compileToJson ((T.unpack root) <> "/") (T.unpack (T.drop 1 path))
+        --   case eitherStatusJson of
+        --     Left errors -> do
+        --       debug $ "got errors"-- <> (T.unpack $ T.decodeUtf8 errors)
+        --       Watchtower.Websocket.broadcastImpl mClients (builderToString (encodeOutgoing (ElmStatus errors)))
 
-            Right jsoutput -> do
-              debug $ "success!"
-              -- Watchtower.Websocket.broadcastImpl mClients ("{\"status\": \"ok\"}")
-              Watchtower.Websocket.broadcastImpl mClients (builderToString (encodeOutgoing (ElmStatus jsoutput)))
+        --     Right jsoutput -> do
+        --       debug $ "success!"
+        --       -- Watchtower.Websocket.broadcastImpl mClients ("{\"status\": \"ok\"}")
+        --       Watchtower.Websocket.broadcastImpl mClients (builderToString (encodeOutgoing (ElmStatus jsoutput)))
 
-      Watchtower.Websocket.runWebSocketsSnap $ Watchtower.Websocket.socketHandler mClients onJoined onReceive (T.decodeUtf8 key)
+      Watchtower.Websocket.runWebSocketsSnap 
+          $ Watchtower.Websocket.socketHandler
+                  mClients onJoined (receive mClients) (T.decodeUtf8 key)
 
     Nothing ->
       error404
 
 builderToString =
   T.decodeUtf8 . Data.ByteString.Lazy.toStrict . Data.ByteString.Builder.toLazyByteString
+
+
+
+receive mClients clientId text =
+  case Json.Decode.fromByteString decodeIncoming (T.encodeUtf8 text) of
+    Left err ->
+      pure ()
+    Right action ->
+      receiveAction mClients clientId action
+
+
+
+
+receiveAction mClients clientId incoming =
+  case incoming of
+    Visible visible ->
+       Watchtower.Websocket.broadcastImpl 
+          mClients 
+          (builderToString (encodeOutgoing (FwdVisible visible)))
+    
+    JumpTo location ->
+      Watchtower.Websocket.broadcastImpl 
+          mClients 
+          (builderToString (encodeOutgoing (FwdJumpTo location)))
+
+    ElmStatusPlease root file -> do
+        eitherStatusJson <- Watchtower.Compile.compileToJson (T.unpack root) (T.unpack file)
+        case eitherStatusJson of
+          Left errors -> do
+            debug $ "got errors"-- <> (T.unpack $ T.decodeUtf8 errors)
+            Watchtower.Websocket.broadcastImpl 
+              mClients 
+              (builderToString (encodeOutgoing (ElmStatus errors)))
+
+          Right jsoutput -> do
+            debug $ "success!"
+            Watchtower.Websocket.broadcastImpl
+              mClients
+              (builderToString (encodeOutgoing (ElmStatus jsoutput)))
+
+
+
+
+decodeIncoming :: Json.Decode.Decoder T.Text Incoming
+decodeIncoming =
+  Json.Decode.field "msg" Json.Decode.string
+    >>= (\msg ->
+            case msg of
+              "StatusPlease" ->
+                  ElmStatusPlease 
+                    <$> Json.Decode.field "root" ((T.pack . Json.String.toChars) <$> Json.Decode.string)
+                    <*> Json.Decode.field "file" ((T.pack . Json.String.toChars) <$> Json.Decode.string)
+
+              "Visible" ->
+                  Visible <$> Watchtower.Details.decodeVisible
+              
+              "Jump" ->
+                  JumpTo <$> Watchtower.Details.decodeLocation
+
+              _ ->
+                  Json.Decode.failure "Unknown msg"
+          
+          
+        )
 
 
 
@@ -126,19 +195,14 @@ There are also one-off questions that can be asked via normal GET requests hande
 -}
 data Incoming
     -- forwarding information from a source to somewhere else
-    = EditorsVisible [ Watchtower.Details.Location ]
-    | EditorActive Watchtower.Details.Location (Maybe Watchtower.Details.Position)
+    = Visible Watchtower.Details.Visible
     | JumpTo Watchtower.Details.Location
-
-    -- status subscription
-    -- remember who calls this and send them status events as they change.
-    | ElmStatusSubscriptionPlease
+    | ElmStatusPlease T.Text T.Text
 
 
 data Outgoing
     -- forwarding information
-    = FwdEditorsVisible [ Watchtower.Details.Location ]
-    | FwdEditorActive Watchtower.Details.Location (Maybe Watchtower.Details.Position)
+    = FwdVisible Watchtower.Details.Visible
     | FwdJumpTo Watchtower.Details.Location
 
     -- new information is available
@@ -149,14 +213,17 @@ encodeOutgoing :: Outgoing -> Data.ByteString.Builder.Builder
 encodeOutgoing out =
   Json.Encode.encodeUgly $
     case out of
-      FwdEditorsVisible locs ->
-        Json.Encode.null
-
-      FwdEditorActive loc pos ->
-        Json.Encode.null
+      FwdVisible visible ->
+        Json.Encode.object
+            [ "msg" ==> Json.Encode.string (Json.String.fromChars "Visible")
+            , "details" ==> Watchtower.Details.encodeVisible visible
+            ]
 
       FwdJumpTo loc ->
-        Json.Encode.null
+        Json.Encode.object
+            [ "msg" ==> Json.Encode.string (Json.String.fromChars "Jump")
+            , "details" ==> Watchtower.Details.encodeLocation loc
+            ]
 
       ElmStatus js ->
         Json.Encode.object
