7d28ca9ff84adb8732fc729b5fd13f9d70b86ba8
Matthew Griffith <mdg.griffith@gmail.com>
2022-01-25 20:07:30 -0500

    formatting

--- ext-watchtower/Watchtower/Questions.hs
+++ ext-watchtower/Watchtower/Questions.hs
@@ -1,38 +1,33 @@
-
 {-# LANGUAGE OverloadedStrings #-}
 
 module Watchtower.Questions where
 
-
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad.Trans (MonadIO (liftIO))
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Builder
+import qualified Data.ByteString.Char8
+import Data.Function ((&))
+import qualified Data.Maybe as Maybe
+import qualified Data.Name as Name
+import qualified Develop.Generate.Help
+import qualified Ext.Common
+import qualified Json.Encode
+import qualified Reporting.Annotation
 import Snap.Core hiding (path)
 import qualified Snap.Util.CORS
-
-import Control.Monad.Trans (MonadIO(liftIO))
-import Control.Applicative ((<$>), (<*>))
-import qualified Data.Maybe as Maybe
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Char8
-import qualified Data.ByteString.Builder
-
-import qualified Json.Encode
-import qualified Watchtower.Details
 import qualified Watchtower.Annotate
+import qualified Watchtower.Details
+import qualified Watchtower.Find
 import qualified Watchtower.Live
-import qualified Develop.Generate.Help
-import qualified Reporting.Annotation
-import qualified Data.Name as Name
-
-
-import qualified Ext.Common
-import Data.Function ((&))
 
 -- One off questions and answers you might have/want.
 data Question
-    = CallgraphPlease  FilePath Name.Name
-    | ListMissingSignaturesPlease FilePath
-    | SignaturePlease FilePath Name.Name
-    | FindDefinitionPlease Watchtower.Details.PointLocation
-    | FindAllInstancesPlease Watchtower.Details.PointLocation
+  = CallgraphPlease FilePath Name.Name
+  | ListMissingSignaturesPlease FilePath
+  | SignaturePlease FilePath Name.Name
+  | FindDefinitionPlease Watchtower.Details.PointLocation
+  | FindAllInstancesPlease Watchtower.Details.PointLocation
 
 -- data Answer
 --     = CallgraphReturned Watchtower.Details.Callgraph
@@ -42,156 +37,138 @@ data Question
 --     | FindAllInstancesReturned
 --     | NOTDONE
 
-
-
-
 serve :: Watchtower.Live.State -> Snap ()
 serve state =
   do
     route
-        [ ("/:action", actionHandler state)
-        ]
+      [ ("/:action", actionHandler state)
+      ]
 
 questionHandler :: Watchtower.Live.State -> Question -> Snap ()
 questionHandler state question =
-    Snap.Util.CORS.applyCORS Snap.Util.CORS.defaultOptions $
-        do
-            answer <- liftIO (ask state question)
+  Snap.Util.CORS.applyCORS Snap.Util.CORS.defaultOptions $
+    do
+      answer <- liftIO (ask state question)
 
-            writeBuilder answer
-
+      writeBuilder answer
 
 actionHandler :: Watchtower.Live.State -> Snap ()
-actionHandler state  =
-    do
-        maybeAction <- getParam "action"
-        case maybeAction of
-            Just "list-missing-signatures" ->
-                do
-                    maybeFile   <- getQueryParam "file"
-                    case maybeFile of
-                        Nothing ->
-                            writeBS "Needs location"
-
-                        Just file -> do
-                            questionHandler state (ListMissingSignaturesPlease (Data.ByteString.Char8.unpack file))
-
-
-            Just "signature" ->
-                do
-                    maybeFile   <- getQueryParam "file"
-                    maybeName   <- getQueryParam "name"
-                    case (maybeFile, maybeName) of
-                        (Just file, Just name) ->
-                            questionHandler state
-                                (SignaturePlease
-                                    (Data.ByteString.Char8.unpack file)
-                                    (Name.fromChars (Data.ByteString.Char8.unpack name))
-                                )
-
-                        _ ->
-                            writeBS "Needs location"
-
-            Just "callgraph" ->
-                do
-                    maybeFile   <- getQueryParam "file"
-                    maybeName   <- getQueryParam "name"
-                    case (maybeFile, maybeName) of
-                        (Just file, Just name) ->
-                            questionHandler state
-                                (CallgraphPlease
-                                    (Data.ByteString.Char8.unpack file)
-                                    (Name.fromChars (Data.ByteString.Char8.unpack name))
-                                )
-
-                        _ ->
-                            writeBS "Needs location"
-
-            -- Just "definition" ->
-            --     do
-            --         maybeLocation   <- getLocation
-            --         case maybeLocation of
-            --             Nothing ->
-            --                 writeBS "Needs location"
-
-            --             Just location ->
-            --                 questionHandler "." (FindDefinitionPlease location)
-
-            -- Just "instances" ->
-            --     do
-            --         maybeLocation   <- getLocation
-            --         case maybeLocation of
-            --             Nothing ->
-            --                 writeBS "Needs location"
-
-            --             Just location ->
-            --                 questionHandler "." (FindAllInstancesPlease location)
-
+actionHandler state =
+  do
+    maybeAction <- getParam "action"
+    case maybeAction of
+      Just "list-missing-signatures" ->
+        do
+          maybeFile <- getQueryParam "file"
+          case maybeFile of
+            Nothing ->
+              writeBS "Needs location"
+            Just file -> do
+              questionHandler state (ListMissingSignaturesPlease (Data.ByteString.Char8.unpack file))
+      Just "signature" ->
+        do
+          maybeFile <- getQueryParam "file"
+          maybeName <- getQueryParam "name"
+          case (maybeFile, maybeName) of
+            (Just file, Just name) ->
+              questionHandler
+                state
+                ( SignaturePlease
+                    (Data.ByteString.Char8.unpack file)
+                    (Name.fromChars (Data.ByteString.Char8.unpack name))
+                )
             _ ->
-                writeBS "Wha?"
+              writeBS "Needs location"
+      Just "callgraph" ->
+        do
+          maybeFile <- getQueryParam "file"
+          maybeName <- getQueryParam "name"
+          case (maybeFile, maybeName) of
+            (Just file, Just name) ->
+              questionHandler
+                state
+                ( CallgraphPlease
+                    (Data.ByteString.Char8.unpack file)
+                    (Name.fromChars (Data.ByteString.Char8.unpack name))
+                )
+            _ ->
+              writeBS "Needs location"
+
+      -- Just "definition" ->
+      --     do
+      --         maybeLocation   <- getLocation
+      --         case maybeLocation of
+      --             Nothing ->
+      --                 writeBS "Needs location"
+
+      --             Just location ->
+      --                 questionHandler "." (FindDefinitionPlease location)
+
+      -- Just "instances" ->
+      --     do
+      --         maybeLocation   <- getLocation
+      --         case maybeLocation of
+      --             Nothing ->
+      --                 writeBS "Needs location"
+
+      --             Just location ->
+      --                 questionHandler "." (FindAllInstancesPlease location)
+
+      _ ->
+        writeBS "Wha?"
 
 getPointLocation :: Snap (Maybe Watchtower.Details.PointLocation)
 getPointLocation =
-    do
-       maybeFilePath <- getQueryParam "file"
-       maybePosition <- getPosition
+  do
+    maybeFilePath <- getQueryParam "file"
+    maybePosition <- getPosition
 
-       let maybeLocation =
-            case (maybeFilePath, maybePosition) of
-                (Just path, Just position) ->
-                    Just
-                        (Watchtower.Details.PointLocation
-                            (Data.ByteString.Char8.unpack path)
-                            position
-                        )
+    let maybeLocation =
+          case (maybeFilePath, maybePosition) of
+            (Just path, Just position) ->
+              Just
+                ( Watchtower.Details.PointLocation
+                    (Data.ByteString.Char8.unpack path)
+                    position
+                )
+            _ ->
+              Nothing
 
-                _ ->
-                    Nothing
-
-       pure maybeLocation
-
+    pure maybeLocation
 
 getPosition :: Snap (Maybe Reporting.Annotation.Position)
 getPosition =
-    do
-        maybeRow <- getQueryParam "row"
-        maybeCol <- getQueryParam "col"
-        let position =
-                (\row col ->
-                    Reporting.Annotation.Position 0 0
-                )
-                <$> maybeRow <*> maybeCol
+  do
+    maybeRow <- getQueryParam "row"
+    maybeCol <- getQueryParam "col"
+    let position =
+          ( \row col ->
+              Reporting.Annotation.Position 0 0
+          )
+            <$> maybeRow <*> maybeCol
 
-        pure position
-
+    pure position
 
 ask :: Watchtower.Live.State -> Question -> IO Data.ByteString.Builder.Builder
 ask state question =
-    case question of
-        CallgraphPlease path name ->
-             do
-                root <- pure (Maybe.fromMaybe "." (Watchtower.Live.getRoot path state))
-                Watchtower.Annotate.callgraph root path name
-                    & fmap Json.Encode.encodeUgly
-
-        ListMissingSignaturesPlease path ->
-            do
-                root <- pure (Maybe.fromMaybe "." (Watchtower.Live.getRoot path state))
-                Ext.Common.debug $ "🛫  List signatures: " ++ show root ++ " <> " ++ show path
-                Watchtower.Annotate.listMissingAnnotations root path
-                    & fmap Json.Encode.encodeUgly
-
-        SignaturePlease path name ->
-             do
-                root <- pure (Maybe.fromMaybe "." (Watchtower.Live.getRoot path state))
-                Watchtower.Annotate.annotation root path name
-                    & fmap Json.Encode.encodeUgly
-
-        FindDefinitionPlease location ->
-            pure (Data.ByteString.Builder.byteString ("NOTDONE"))
-
-        FindAllInstancesPlease location ->
-            pure (Data.ByteString.Builder.byteString ("NOTDONE"))
-
-
-
+  case question of
+    CallgraphPlease path name ->
+      do
+        root <- pure (Maybe.fromMaybe "." (Watchtower.Live.getRoot path state))
+        Watchtower.Annotate.callgraph root path name
+          & fmap Json.Encode.encodeUgly
+    ListMissingSignaturesPlease path ->
+      do
+        root <- pure (Maybe.fromMaybe "." (Watchtower.Live.getRoot path state))
+        Ext.Common.debug $ "🛫  List signatures: " ++ show root ++ " <> " ++ show path
+        Watchtower.Annotate.listMissingAnnotations root path
+          & fmap Json.Encode.encodeUgly
+    SignaturePlease path name ->
+      do
+        root <- pure (Maybe.fromMaybe "." (Watchtower.Live.getRoot path state))
+        Watchtower.Annotate.annotation root path name
+          & fmap Json.Encode.encodeUgly
+    FindDefinitionPlease location ->
+      pure (Data.ByteString.Builder.byteString ("NOTDONE"))
+    FindAllInstancesPlease location ->
+      pure (Data.ByteString.Builder.byteString ("NOTDONE"))
