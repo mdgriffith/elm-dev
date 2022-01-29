3cc0dd84a85026c9030c2e985063e2cc9498533b
Matthew Griffith <mdg.griffith@gmail.com>
2022-01-29 13:26:04 -0500

    formatting

--- ext-sentry/Ext/Filewatch.hs
+++ ext-sentry/Ext/Filewatch.hs
@@ -2,51 +2,51 @@
 
 module Ext.Filewatch where
 
-import Ext.Common
-import System.FSNotify
-import Control.Concurrent (threadDelay, forkIO)
+import Control.Concurrent (forkIO, threadDelay)
+import qualified Control.FoldDebounce as Debounce
 import Control.Monad (forever)
 import qualified Data.List as List
-import qualified Control.FoldDebounce as Debounce
+import Ext.Common
+import System.FSNotify
 
-
+watch :: FilePath -> ([FilePath] -> IO ()) -> IO ()
 watch root action =
-  trackedForkIO $ withManager $ \mgr -> do
-    trigger <-
-      Debounce.new
-        Debounce.Args
-          { Debounce.cb = (\events -> action events)
-          , Debounce.fold = (\l v -> List.nub $ v:l)
-          , Debounce.init = []
-          }
-        Debounce.def
-          { Debounce.delay = 10000 -- 10ms
-          , Debounce.alwaysResetTimer = True
-          }
+  trackedForkIO $
+    withManager $ \mgr -> do
+      trigger <-
+        Debounce.new
+          Debounce.Args
+            { Debounce.cb = (\events -> action events),
+              Debounce.fold = (\l v -> List.nub $ v : l),
+              Debounce.init = []
+            }
+          Debounce.def
+            { Debounce.delay = 10000, -- 10ms
+              Debounce.alwaysResetTimer = True
+            }
 
-    -- start a watching job (in the background)
-    watchTree
-      mgr          -- manager
-      root         -- directory to watch
-      (const True) -- predicate
-      (\e -> do
-        let
-          f = case e of
-                Added f _ _ -> f
-                Modified f _ _ -> f
-                Removed f _ _ -> f
-                Unknown f _ _ -> f
+      -- start a watching job (in the background)
+      watchTree
+        mgr -- manager
+        root -- directory to watch
+        (const True) -- predicate
+        ( \e -> do
+            let f = case e of
+                  Added f _ _ -> f
+                  Modified f _ _ -> f
+                  Removed f _ _ -> f
+                  Unknown f _ _ -> f
 
-          shouldRefresh = do
-                not (List.isInfixOf ".git" f)
-             && not (List.isInfixOf "elm-stuff" f)
+                shouldRefresh =
+                  do
+                    not (List.isInfixOf ".git" f)
+                    && not (List.isInfixOf "elm-stuff" f)
 
-        if shouldRefresh
-          then do
-            Debounce.send trigger f
-          else
-            pure ()
-      )
+            if shouldRefresh
+              then do
+                Debounce.send trigger f
+              else pure ()
+        )
 
-    -- sleep forever (until interrupted)
-    forever $ threadDelay 1000000
+      -- sleep forever (until interrupted)
+      forever $ threadDelay 1000000
