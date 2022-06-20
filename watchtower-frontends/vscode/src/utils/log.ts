b0ca0df48a5631cda499576e80b3c95df8e36a25
Matthew Griffith <mdg.griffith@gmail.com>
2022-06-20 11:27:03 -0400

    improve log.obj output

--- watchtower-frontends/vscode/src/utils/log.ts
+++ watchtower-frontends/vscode/src/utils/log.ts
@@ -9,6 +9,37 @@ export function log(str) {
 export function obj(name, o) {
   watchtower.appendLine(name);
   for (const [key, value] of Object.entries(o)) {
-    watchtower.appendLine("  " + key + ": " + value);
+
+    if (Array.isArray(value)) {
+      let first = true
+      watchtower.appendLine("  " + key);
+      if (value.length == 0) {
+        watchtower.appendLine("    []");
+      } else {
+        for (const subval in value) {
+          if (first) {
+            watchtower.appendLine("    [ " + subval);
+            first = false
+          } else {
+            watchtower.appendLine("    , " + subval);
+          }
+          
+        }
+        watchtower.appendLine("    ]");
+      }
+    } else if (
+      typeof value === 'object' &&
+      value !== null
+    ) {
+      watchtower.appendLine("  " + key + ": ");
+      watchtower.appendLine(Object.keys(value).join("\n"));
+      for (const [subKey, subValue] of Object.entries(value)) {
+        watchtower.appendLine("    " + subKey + ": " + subValue);
+      }
+
+    } else {
+      watchtower.appendLine("  " + key + ": " + value);
+    }
+    
   }
 }
