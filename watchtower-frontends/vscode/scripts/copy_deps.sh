a503c8843a0bea01cb1fd3c0365fbfc06ecd6fce
Matthew Griffith <mdg.griffith@gmail.com>
2022-09-15 10:56:56 -0400

    add elm-codegen to build process

---
+++ watchtower-frontends/vscode/scripts/copy_deps.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+
+
+cp ./src/interactive/elm.json ./out/interactive/elm.json
+cp ./node_modules/elm-format/unpacked_bin/elm-format ./out/elm-format
+cp ./node_modules/elm/bin/elm ./out/elm
