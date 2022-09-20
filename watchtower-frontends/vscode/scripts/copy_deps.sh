e6c2603b2bdb342a658be0ac96bd7db30e0b5a80
Matthew Griffith <mdg.griffith@gmail.com>
2022-09-20 15:44:38 -0400

    don't fail if the files are already there

--- watchtower-frontends/vscode/scripts/copy_deps.sh
+++ watchtower-frontends/vscode/scripts/copy_deps.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 
-mkdir ./out/interactive
-mkdir ./out/interactive/src
+mkdir -p ./out/interactive
+mkdir -p ./out/interactive/src
 cp ./src/interactive/elm.json ./out/interactive/elm.json
 cp -r ./codegen/helpers/* ./out/interactive/src
 cp ./node_modules/elm-format/unpacked_bin/elm-format ./out/elm-format
