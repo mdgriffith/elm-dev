e9d843909d61047bc3f07dc66f2be6f95a6e8f67
Matthew Griffith <mdg.griffith@gmail.com>
2022-11-20 13:39:34 -0500

    Simplify the compile path for Live.Compile

--- ext-watchtower/Watchtower/Live/Compile.hs
+++ ext-watchtower/Watchtower/Live/Compile.hs
@@ -7,6 +7,7 @@ module Watchtower.Live.Compile (compileAll, recompile) where
 import qualified Data.NonEmptyList as NonEmpty
 import qualified Data.List as List
 import qualified Data.Set as Set
+import qualified Data.Maybe as Maybe
 import qualified Data.ByteString.Lazy
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
@@ -15,7 +16,7 @@ import qualified Data.ByteString.Builder
 import qualified Control.Concurrent.STM as STM
 
 import qualified Reporting.Render.Type.Localizer
-import Control.Monad as Monad (foldM, guard)
+import Control.Monad as Monad (foldM, guard, mapM_)
 
 import qualified Ext.Sentry
 import Ext.Common
@@ -42,24 +43,23 @@ compileAll (Client.State mClients mProjects) = do
   trackedForkIO $
     track "recompile all projects" $ do
       projects <- STM.readTVarIO mProjects
-      Monad.foldM
-        (\files proj -> compileProject mClients proj)
-        []
+      Monad.mapM_
+        (compileProject mClients)
         projects
 
       Ext.Common.log "🛬"  "Recompile everything finished"
 
 
-compileProject :: STM.TVar [Client.Client] -> Client.ProjectCache -> IO [FilePath]
+compileProject :: STM.TVar [Client.Client] -> Client.ProjectCache -> IO ()
 compileProject mClients proj@(Client.ProjectCache (Ext.Dev.Project.Project projectRoot entrypoints) cache) =
   case entrypoints of
     [] ->
       do
         Ext.Common.log "Skipping compile, no entrypoint" projectRoot
-        pure []
+        pure ()
 
-    _ ->
-        recompileChangedFile mClients entrypoints proj
+    topEntry : remainEntry ->
+        recompileFile mClients (topEntry, remainEntry, proj)
 
 
 
@@ -77,144 +77,109 @@ recompile (Client.State mClients mProjects) allChangedFiles = do
   let changedElmFiles = List.filter (\filepath -> ".elm" `List.isSuffixOf` filepath ) allChangedFiles
   if (changedElmFiles /= [])
     then do
-      debug $ "🛫  recompile starting: " ++ show changedElmFiles
+      
       projects <- STM.readTVarIO mProjects
+      let affectedProjects = Maybe.mapMaybe (toAffectedProject changedElmFiles) projects
+      case affectedProjects of
+          [] -> 
+              Ext.Common.log "No affected projects" "!"
+          _ ->
+              pure ()
+      
       trackedForkIO $
         track "recompile" $ do
-          Monad.foldM
-            (recompileChangedFile mClients)
-            changedElmFiles
-            projects
+          
+          -- send down status for 
+          Monad.mapM_
+            (recompileFile mClients)
+            affectedProjects
+          
+          -- send down warnings and docs
+          Monad.mapM_
+            (sendInfo mClients)
+            affectedProjects
 
-          debug $ "🛬  recompile finished: " ++ show changedElmFiles
+          -- Get the status of the entire project
+          -- Monad.foldM
+          --   (recompileProject mClients)
+          --   affectedProjects
+
+
     else
         pure ()
 
 
 
-recompileChangedFile :: STM.TVar [Client.Client] -> [String] -> Client.ProjectCache -> IO [FilePath]
-recompileChangedFile mClients changedFiles projCache@(Client.ProjectCache proj@(Ext.Dev.Project.Project projectRoot entrypoints) cache) =
-    do
+
+
+toAffectedProject :: [String] -> Client.ProjectCache -> Maybe (String, [String], Client.ProjectCache)
+toAffectedProject changedFiles projCache@(Client.ProjectCache proj@(Ext.Dev.Project.Project projectRoot entrypoints) cache) =
       case changedFiles of
         [] ->
-          do
-            pure []
+          Nothing
+
         (top : remain) ->
-            if List.any (\f -> Ext.Dev.Project.contains f proj) changedFiles then
-              do
+          if List.any (\f -> Ext.Dev.Project.contains f proj) changedFiles then
+            Just (top, remain, projCache)
 
-                  let entry = NonEmpty.List top remain
-
-                  -- Compile all changed files
-                  eitherStatusJson <-
-                    compileMode
-                      projectRoot
-                      entry
-
-                  Ext.Sentry.updateCompileResult cache $
-                    pure eitherStatusJson
+          else
+              Nothing
 
 
-                  -- Recompile the entire project
-                  recompileProjectIfSubFile mClients changedFiles projCache
-
-                  (Ext.Dev.Info warnings docs) <- Ext.Dev.info projectRoot top
-                  case warnings of
-                    Nothing -> pure ()
-                    
-                    Just (sourceMod, warns) -> do
-                      Ext.Common.log ("Sending down " <> show (length warnings) <> " warnings") "!"
-                      Client.broadcast mClients
-                        (Client.Warnings top (Reporting.Render.Type.Localizer.fromModule sourceMod) warns)
-                      pure ()
-
-                  
-                  case docs of
-                    Nothing -> pure ()
-
-                    Just docs -> do
-                      Ext.Common.log "Sending down docs" "!"
-                      Client.broadcast mClients
-                        (Client.Docs top [ docs ])
-                      pure ()
-
-                   -- Send compilation status
-                  case eitherStatusJson of
-                    Right statusJson ->
-                      pure ()
-
-                    Left errJson ->
-                      do
-                        Ext.Common.log "Changed file failed" "!"
-                        Client.broadcast mClients
-                          (Client.ElmStatus [ Client.ProjectStatus proj False errJson ])
-
-                        pure ()
-
-                  pure []
-
-            else
-                pure []
 
 
-{-
-This function will recompile a project using the projects entrypoint if the changed file is within a projects root dir.
-
-This does mean that if the detected entrypoint doesn't ultimately import the changed file, then you won't get errors
-
--}
-recompileProjectIfSubFile :: STM.TVar [Client.Client] -> [String] -> Client.ProjectCache -> IO [FilePath]
-recompileProjectIfSubFile mClients remainingFiles (Client.ProjectCache proj@(Ext.Dev.Project.Project projectRoot entrypoints) cache) =
+recompileFile ::  STM.TVar [Client.Client] -> (String, [String], Client.ProjectCache) -> IO ()
+recompileFile mClients ( top, remain , projCache@(Client.ProjectCache proj@(Ext.Dev.Project.Project projectRoot entrypoints) cache)) =
     do
-      let remaining =
-            List.filter
-              (\file -> not (Ext.Dev.Project.contains file proj))
-              remainingFiles
 
-      let maybeEntry =
-            case entrypoints of
-              [] ->
-                let filesWithinProject =
-                      List.filter
-                        (\file -> Ext.Dev.Project.contains file proj)
-                        remainingFiles
-                  in case filesWithinProject of
-                      [] -> Nothing
-                      top : remain ->
-                        Just (NonEmpty.List top remain)
-              -- Nothing
-              top : remainingEntrypoints ->
-                Just (NonEmpty.List top remainingEntrypoints)
+        let entry = NonEmpty.List top remain
 
-      case maybeEntry of
-        Nothing ->
-          do
-            debug $ ("☹️ No detected affected project")
-            pure remaining
-        Just entry ->
-          do
-            -- Can compileToJson take multiple entrypoints like elm make?
-            eitherStatusJson <-
-              compileMode
-                projectRoot
-                entry
+        -- Compile all changed files
+        eitherStatusJson <-
+          compileMode
+            projectRoot
+            entry
 
-            Ext.Sentry.updateCompileResult cache $
-              pure eitherStatusJson
+        Ext.Sentry.updateCompileResult cache $
+          pure eitherStatusJson
 
-            case eitherStatusJson of
-              Right statusJson ->
-                do
-                  Ext.Common.log "Affected project success" "--"
-                  Client.broadcast mClients
-                    (Client.ElmStatus [ Client.ProjectStatus proj True statusJson ])
+        -- Send compilation status
+        case eitherStatusJson of
+          Right statusJson ->
+            do 
+              Ext.Common.log "File successfully compiles" "!"
+              pure ()
 
-              Left errJson ->
-                -- send the errors to any client that's listening
-                do
-                  Ext.Common.log "Affected project failure" "--"
-                  Client.broadcast mClients
-                    (Client.ElmStatus [ Client.ProjectStatus proj False errJson ])
+          Left errJson ->
+            do
+              Ext.Common.log "Changed file failed" "!"
+              Client.broadcast mClients
+                (Client.ElmStatus [ Client.ProjectStatus proj False errJson ])
+
+              pure ()
 
 
-            pure remaining
+sendInfo ::  STM.TVar [Client.Client] -> (String, [String], Client.ProjectCache) -> IO ()
+sendInfo mClients ( top, remain , projCache@(Client.ProjectCache proj@(Ext.Dev.Project.Project projectRoot entrypoints) cache)) =
+    do
+        (Ext.Dev.Info warnings docs) <- Ext.Dev.info projectRoot top
+
+        case warnings of
+          Nothing -> pure ()
+          
+          Just (sourceMod, warns) -> do
+            Ext.Common.log ("Sending down " <> show (length warnings) <> " warnings") "!"
+            Client.broadcast mClients
+              (Client.Warnings top (Reporting.Render.Type.Localizer.fromModule sourceMod) warns)
+            pure ()
+
+        case docs of
+          Nothing -> pure ()
+
+          Just docs -> do
+            Ext.Common.log "Sending down docs" "!"
+            Client.broadcast mClients
+              (Client.Docs top [ docs ])
+            pure ()
+
+  
