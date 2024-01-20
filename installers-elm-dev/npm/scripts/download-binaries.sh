de07665f1198b60a2712f00770961499c7b57ae4
Matthew Griffith <mdg.griffith@gmail.com>
2024-01-20 09:28:04 -0500

    Add download binaries script

---
+++ installers-elm-dev/npm/scripts/download-binaries.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+VERSION="0.1.0"
+
+# Define URLs for download
+URL_LINUX_ARM64="https://static.lamdera.com/bin/elm-dev/elm-dev-next-linux-arm64"
+URL_LINUX_X86_64="https://static.lamdera.com/bin/elm-dev/elm-dev-next-linux-x86_64"
+URL_MACOS_ARM64="https://static.lamdera.com/bin/elm-dev/elm-dev-next-macos-arm64"
+URL_MACOS_X86_64="https://static.lamdera.com/bin/elm-dev/elm-dev-next-macos-x86_64"
+URL_WINDOWS_X86_64="https://static.lamdera.com/bin/elm-dev/elm-dev-next-windows-x86_64.zip"
+
+# Create target directories
+mkdir -p packages/linux_arm64
+mkdir -p packages/linux_x64
+mkdir -p packages/darwin_arm64
+mkdir -p packages/darwin_x64
+mkdir -p packages/win32_x64
+
+# Download and save files to the respective directories
+curl -L $URL_LINUX_ARM64 -o packages/linux_arm64/elm-dev
+curl -L $URL_LINUX_X86_64 -o packages/linux_x64/elm-dev
+curl -L $URL_MACOS_ARM64 -o packages/darwin_arm64/elm-dev
+curl -L $URL_MACOS_X86_64 -o packages/darwin_x64/elm-dev
+curl -L $URL_WINDOWS_X86_64 -o packages/win32_x64/elm-dev.zip
+
+# Unzip the Windows file and rename to elm-dev.exe
+unzip packages/win32_x64/elm-dev.zip -d packages/win32_x64
+mv packages/win32_x64/elm-dev/elm-dev.exe packages/win32_x64/elm-dev.exe
+rm packages/win32_x64/elm-dev.zip # Remove the zip file after extraction
+rm packages/win32_x64/elm-dev # Remove the directory after extraction
+
+# Make all files executable
+chmod +x packages/linux_arm64/elm-dev
+chmod +x packages/linux_x64/elm-dev
+chmod +x packages/darwin_arm64/elm-dev
+chmod +x packages/darwin_x64/elm-dev
+chmod +x packages/win32_x64/elm-dev.exe
+
+
+# Update version in package.json for each directory
+for dir in packages/linux-arm64 packages/linux-x64 packages/darwin-arm64 packages/darwin-x64 packages/win32-x64; do
+    if [ -f "$dir/package.json" ]; then
+        # Use jq to update the version. jq must be installed.
+        jq --arg v "$VERSION" '.version = $v' "$dir/package.json" > tmp.json && mv tmp.json "$dir/package.json"
+    fi
+done
