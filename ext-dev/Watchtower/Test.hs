59d5d1c1e5da8ef2f74d013bf53c17df1618cb45
Matthew Griffith <mdg.griffith@gmail.com>
2024-08-03 10:34:45 -0400

    Run test server in disk mode while things are ironed out

--- ext-dev/Watchtower/Test.hs
+++ ext-dev/Watchtower/Test.hs
@@ -1,14 +1,14 @@
 module Watchtower.Test where
 
 import Control.Concurrent (threadDelay)
+import qualified Control.Exception
+import Ext.Common
+import qualified Ext.CompileMode
+import qualified Ext.Log
 import qualified System.Directory as Dir
 import qualified System.Environment as Env
-
-import Ext.Common
-import qualified Ext.Log
 import qualified Watchtower.Server
-import qualified Control.Exception
-import qualified Ext.CompileMode
+
 {-
 
 Allows us to use `:rr` in GHCI to quickly kill, typecheck, rebuild & reboot the server,
@@ -16,15 +16,18 @@ without having to build a binary and reboot it in shell.
 
 -}
 
+serve :: IO ()
 serve = do
-  projectDir <- Control.Exception.catch (Env.lookupEnv "ELM_WATCHTOWER_START_PROJECT")
-                  (const $ pure Nothing ::  Control.Exception.IOException -> IO (Maybe String))
-  -- Ext.CompileMode.setModeDisk
-  Ext.CompileMode.setModeMemory
-  trackedForkIO $
-    Ext.Log.withAllBut 
-      [ -- Add any flags to exclude here!
-      ] $
-      Watchtower.Server.serve
-          projectDir
-          (Watchtower.Server.Flags (Just 51213))
+  projectDir <-
+    Control.Exception.catch
+      (Env.lookupEnv "ELM_WATCHTOWER_START_PROJECT")
+      (const $ pure Nothing :: Control.Exception.IOException -> IO (Maybe String))
+  Ext.CompileMode.setModeDisk
+  -- Ext.CompileMode.setModeMemory
+  trackedForkIO
+    $ Ext.Log.withAllBut
+      []
+    -- Add any flags to exclude here!
+    $ Watchtower.Server.serve
+      projectDir
+      (Watchtower.Server.Flags (Just 51213))
