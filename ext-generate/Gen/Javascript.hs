bf9e59d23d3ce11f807f883e1a5b7a54c2ee8893
Matthew Griffith <mdg.griffith@gmail.com>
2025-02-25 11:21:28 -0500

    Add ability to run js through bun

---
+++ ext-generate/Gen/Javascript.hs
@@ -0,0 +1,34 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+module Gen.Javascript where
+
+import qualified Language.Haskell.TH
+import qualified Data.FileEmbed
+import Language.Haskell.TH.Syntax
+import System.Process (readCreateProcess, shell)
+import Control.Exception (try, SomeException)
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as C8
+import System.IO.Temp (withSystemTempFile)
+import System.FilePath ((</>))
+
+-- | Load a file at compile time
+generatorJs :: BS.ByteString
+generatorJs =
+     $(Data.FileEmbed.bsToExp =<< 
+        Language.Haskell.TH.runIO 
+            (BS.readFile ("ext-generate" </> "generator" </> "dist" </> "run.js"))
+    )
+
+-- | Execute embedded JavaScript using Bun
+
+run :: BS.ByteString -> IO (Either String String)
+run jsCode = withSystemTempFile "embedded.js" $ \tempPath handle -> do
+    -- Write the embedded code to a temporary file
+    BS.hPut handle jsCode
+    -- Execute using Bun
+    result <- try $ readCreateProcess (shell $ "bun " ++ tempPath) ""
+    case result of
+        Left err -> return $ Left $ "Error executing script: " ++ show (err :: SomeException)
+        Right output -> return $ Right output
