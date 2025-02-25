586addec391982fdf67ffe3bd92b974dd944bd92
Matthew Griffith <mdg.griffith@gmail.com>
2025-02-25 15:06:05 -0500

    build script

--- ext-generate/generator/build.sh
+++ ext-generate/generator/build.sh
@@ -4,5 +4,12 @@ rm -rf Gen;
 elm-codegen install;
 cd ..;
 
+
+bun run typecheck
+
 rm -rf dist
 elm make main/Run.elm --output=dist/generate.js
+bun build ./index.ts --outfile=dist/run.js
+
+# To reduce confusion, remove the intermediate file.
+rm ./dist/generate.js
