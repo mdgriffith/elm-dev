8021e0e97e9572088827b5943d95ec8864e200fb
Matthew Griffith <mdg.griffith@gmail.com>
2025-03-02 09:42:08 -0500

    Implement RunConfig.toRunConfig

--- ext-generate/Gen/RunConfig.hs
+++ ext-generate/Gen/RunConfig.hs
@@ -9,17 +9,17 @@ import GHC.Generics
 import qualified Data.ByteString as BS
 import qualified Data.List as List
 import qualified Data.Map as Map
-
-
-
-
-
-
+import qualified System.Directory as Dir
+import qualified System.FilePath as FilePath
+import Control.Monad (forM)
+import qualified Data.Text.IO as TextIO
+import qualified Data.Text.Encoding as TextEnc
+import qualified Data.Text as Text
 
 data RunConfig = RunConfig
     { app :: Maybe App
     , docs :: Maybe Docs
-    , assets :: Maybe Assets
+    , assets :: Maybe [AssetGroup]
     , theme :: Maybe Config.Theme
     } deriving (Generic)
 
@@ -36,8 +36,8 @@ instance FromJSON App
 instance ToJSON App
 
 data Page = Page
-    { id :: String
-    , moduleName :: [String]
+    { _id :: String
+    , _moduleName :: [String]
     , urlOnly :: Bool
     , route :: Maybe Route
     } deriving (Generic)
@@ -86,10 +86,6 @@ instance FromJSON Guide
 instance ToJSON Guide
 
 
-data Assets = List AssetGroup deriving (Generic)
-
-instance FromJSON Assets
-instance ToJSON Assets
 
 data AssetGroup = AssetGroup
     { name :: String 
@@ -100,5 +96,107 @@ data AssetGroup = AssetGroup
 
 instance FromJSON AssetGroup
 instance ToJSON AssetGroup
+
+toRunConfig :: FilePath -> Config.Config -> IO RunConfig
+toRunConfig cwd config = do
+    -- Convert stores by reading store files
+    storeIds <- findStores (cwd FilePath.</> "src" FilePath.</> "app" FilePath.</> "Store")
+    
+    let stores = map (\sid -> Store { storeId = sid }) storeIds
+    
+    -- Convert pages from Config.AppConfig
+    let pages = case Config.configApp config of
+            Nothing -> []
+            Just appConfig -> 
+                Map.foldrWithKey (\k v acc -> 
+                    let moduleName = Text.splitOn "." k
+                        route = if Config.pageUrlOnly v
+                            then Nothing
+                            else Just $ Route 
+                                { routeId = Text.unpack k
+                                , url = Text.unpack (Config.pageUrl v)
+                                , redirectFrom = map Text.unpack (Config.pageRedirectFrom v)
+                                }
+                    in Page 
+                        { _id = Text.unpack k
+                        , _moduleName = map Text.unpack moduleName
+                        , urlOnly = Config.pageUrlOnly v
+                        , route = route
+                        } : acc
+                ) [] (Config.appPages appConfig)
+
+    -- Convert docs
+    docs <- case Config.configDocs config of
+        Nothing -> return Nothing
+        Just docsConfig -> do
+            let docsPath = cwd FilePath.</> Text.unpack (Config.docsSrc docsConfig)
+            
+            -- Read README if it exists
+            readmeContent <- tryReadFile (docsPath FilePath.</> "README.md")
+            
+            -- Read guides
+            guides <- case Config.docsGuides docsConfig of
+                Nothing -> return []
+                Just guidePaths -> forM (map Text.unpack guidePaths) $ \guidePath -> do
+                    content <- tryReadFile (docsPath FilePath.</> guidePath FilePath.<.> "md")
+                    return Guide 
+                        { guideName = FilePath.dropExtension guidePath
+                        , guideContent = content
+                        }
+            
+            return $ Just Docs
+                { readme = readmeContent
+                , guides = guides
+                , project = Data.Aeson.Null  -- Placeholder, would need project documentation
+                , modules = []  -- Placeholder, would need module documentation
+                , deps = Map.empty  -- Placeholder, would need dependency documentation
+                }
+
+    -- Convert assets
+    assets <- case Config.configAssets config of
+        Nothing -> return Nothing
+        Just assetConfigs -> do
+            assetGroups <- forM (Map.toList assetConfigs) $ \(name, conf) -> do
+                let srcPath = Text.unpack (Config.assetSrc conf)
+                    serverPath = Text.unpack (Config.assetOnServer conf)
+                    crumbPath = FilePath.splitPath srcPath
+                
+                content <- tryReadFile (cwd FilePath.</> srcPath)
+                return AssetGroup
+                    { name = Text.unpack name
+                    , crumbs = filter (not . null) $ map FilePath.dropTrailingPathSeparator crumbPath
+                    , pathOnServer = serverPath
+                    , content = maybe "" id content
+                    }
+            
+            return (Just assetGroups)
+    
+    return RunConfig
+        { app = if null pages && null stores 
+                then Nothing 
+                else Just App { pages = pages, stores = stores }
+        , docs = docs
+        , assets = assets
+        , theme = Config.configTheme config
+        }
+
+-- | Helper function to safely read a file, returning Nothing if the file doesn't exist
+tryReadFile :: FilePath -> IO (Maybe String)
+tryReadFile path = do
+    exists <- Dir.doesFileExist path
+    if exists
+        then Just <$> readFile path
+        else return Nothing
+
+-- | Find all store files in the given directory and return their names without extension
+findStores :: FilePath -> IO [String]
+findStores dir = do
+    exists <- Dir.doesDirectoryExist dir
+    if not exists
+        then return []
+        else do
+            files <- Dir.listDirectory dir
+            return $ map FilePath.dropExtension $ 
+                    filter (\f -> FilePath.takeExtension f == ".elm") files
 
 
