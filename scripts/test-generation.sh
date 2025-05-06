14ee40f0808a7271433db56f1f92bb2caae82caf
Matthew Griffith <mdg.griffith@gmail.com>
2025-05-06 16:43:05 -0400

    Add new test project script

---
+++ scripts/test-generation.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+
+
+# Clear the output directory and elm stuff
+rm -rf playground
+mkdir -p playground
+
+# Create a new project
+stack run --cwd playground -- init
+
+# Run the generation
+stack run --cwd playground -- make src/app/Main.elm
