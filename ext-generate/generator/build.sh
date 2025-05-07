cd757e033c6d8715bcfe6f907566fc24df716fe8
Matthew Griffith <mdg.griffith@gmail.com>
2025-05-06 20:41:31 -0400

    Use bash script for building

--- ext-generate/generator/build.sh
+++ ext-generate/generator/build.sh
@@ -8,7 +8,7 @@ cd ..;
 bun run typecheck
 
 rm -rf dist
-elm make main/Run.elm --output=dist/generate.js
+elm make main/Run.elm --output=dist/generate.js --optimize
 bun build ./index.ts --outfile=dist/run.js
 
 # To reduce confusion, remove the intermediate file.
