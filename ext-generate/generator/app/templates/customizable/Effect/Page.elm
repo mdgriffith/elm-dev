d63a64aab71f844c4d7baf5ee68b56823ab44fed
Matthew Griffith <mdg.griffith@gmail.com>
2025-05-11 17:21:45 -0400

    Some clarifying comments for Effect.Page

--- ext-generate/generator/app/templates/customizable/Effect/Page.elm
+++ ext-generate/generator/app/templates/customizable/Effect/Page.elm
@@ -16,13 +16,15 @@ import App.View.Region
 import Effect exposing (Effect)
 
 
-{-| -}
+{-| Load the data for a page, but don't show it yet.
+-}
 preload : App.Page.Id.Id -> Effect msg
 preload =
     Effect.Preload
 
 
-{-| -}
+{-| Load the page (if necessary) and view a page at a specific region
+-}
 loadAt : App.View.Region.Region -> App.Page.Id.Id -> Effect msg
 loadAt region pageId =
     Effect.ViewUpdated (App.View.Region.Push region pageId)
