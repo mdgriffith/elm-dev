a8503563ea94ee34660bbd7c2c4c08708860355d
Matthew Griffith <mdg.griffith@gmail.com>
2025-05-13 17:30:34 -0400

    import generated css

--- ext-generate/generator/app/templates/toJs/main.ts
+++ ext-generate/generator/app/templates/toJs/main.ts
@@ -5,7 +5,7 @@ import * as Ports from "./js/ports";
 import Webcomponents from "./js/webcomponents";
 
 // Import all generated CSS files
-import.meta.glob("../.elm-prefab/**/*.css", { eager: true });
+import.meta.glob("../elm-stuff/generated/**/*.css", { eager: true });
 
 // Include any custom elements we need.
 Webcomponents();
