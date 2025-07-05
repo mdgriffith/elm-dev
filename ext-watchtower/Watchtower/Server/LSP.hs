2b4077f299cc4add211a0a27c9131ef1e9a4b8e0
Matthew Griffith <mdg.griffith@gmail.com>
2025-07-05 09:49:57 -0400

    Remove old MCP stuff

---
+++ ext-watchtower/Watchtower/Server/LSP.hs
@@ -0,0 +1,27 @@
+module Watchtower.Server.LSP (serve) where
+
+import qualified Data.Aeson as JSON
+import qualified Data.Text as Text
+import qualified Watchtower.Live as Live
+import qualified Watchtower.Server.JSONRPC as JSONRPC
+
+success reqId result =
+  Right (JSONRPC.success reqId result)
+
+err reqId str =
+  Left (JSONRPC.err reqId (Text.pack str))
+
+-- | Example handler that demonstrates the interface
+serve :: Live.State -> JSONRPC.Request -> IO (Either JSONRPC.Error JSONRPC.Response)
+serve _state req@(JSONRPC.Request _ reqId method params) = do
+  case Text.unpack method of
+    "ping" -> do
+      return $ success reqId (JSON.String (Text.pack "pong"))
+    "echo" -> do
+      let result = case params of
+            Just p -> p
+            Nothing -> JSON.Null
+      return $ success reqId result
+    _ -> do
+      return $
+        (Left (JSONRPC.errorMethodNotFound reqId method))
