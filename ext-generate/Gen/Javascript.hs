dbbaa22d215dccd72d6025060b706f87f9e922a9
Matthew Griffith <mdg.griffith@gmail.com>
2025-07-26 11:18:50 -0400

    Formatting

--- ext-generate/Gen/Javascript.hs
+++ ext-generate/Gen/Javascript.hs
@@ -1,42 +1,47 @@
-{-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -Wall #-}
+
 module Gen.Javascript where
 
-import qualified Language.Haskell.TH
-import qualified Data.FileEmbed
-import System.Process (readCreateProcess, shell, StdStream(..), withCreateProcess, waitForProcess, std_in, std_out)
-import Control.Exception (try, SomeException)
+import Control.Exception (SomeException, try)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.UTF8 as UTF8
-import System.IO.Temp (withSystemTempFile)
+import qualified Data.FileEmbed
+import qualified Language.Haskell.TH
 import System.FilePath ((</>))
-import System.Exit (ExitCode(..))
-import System.IO (hClose, hGetContents)
+import System.IO (hClose)
+import System.IO.Temp (withSystemTempFile)
+import qualified System.Process
 
 -- | Load a file at compile time
 generatorJs :: BS.ByteString
 generatorJs =
-     $(Data.FileEmbed.bsToExp =<< 
-        Language.Haskell.TH.runIO 
-            (BS.readFile ("ext-generate" </> "generator" </> "dist" </> "run.js"))
-     )
+  $( Data.FileEmbed.bsToExp
+       =<< Language.Haskell.TH.runIO
+         (BS.readFile ("ext-generate" </> "generator" </> "dist" </> "run.js"))
+   )
 
+interactiveJs :: BS.ByteString
+interactiveJs =
+  $( Data.FileEmbed.bsToExp
+       =<< Language.Haskell.TH.runIO
+         (BS.readFile ("ext-generate" </> "generator" </> "dist" </> "interactive-run.js"))
+   )
 
 -- | Execute embedded JavaScript using Bun
-run :: BS.ByteString -> BS.ByteString ->  IO (Either String String)
+run :: BS.ByteString -> BS.ByteString -> IO (Either String String)
 run jsCode input = withSystemTempFile "embedded.js" $ \tempPath handle -> do
-    -- Write the embedded code to a temporary file
-    BS.hPut handle jsCode
-    hClose handle  -- Close the handle after writing
-    -- Execute using Bun and pass input through stdin
-    let process = shell $ "node " ++ tempPath
-    result <- try $ readCreateProcess process (UTF8.toString input)
-    case result of
-        Left err -> return $ Left $ "Error executing script: " ++ show (err :: SomeException)
-        Right output -> return $ Right output
-
+  -- Write the embedded code to a temporary file
+  BS.hPut handle jsCode
+  hClose handle -- Close the handle after writing
+  -- Execute using Bun and pass input through stdin
+  let process = System.Process.shell $ "node " ++ tempPath
+  result <- try $ System.Process.readCreateProcess process (UTF8.toString input)
+  case result of
+    Left err -> return $ Left $ "Error executing script: " ++ show (err :: SomeException)
+    Right output -> return $ Right output
 
 -- Dynamically adjusted by build.sh to make sure haskell doesn't bamboozle us.
 version :: String
-version = "c4e6cfb356d5239f"
+version = "cccbfe74d732b858"
