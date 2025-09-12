cdad9e0bcaa46dd698299112eec4ffc72883d4bc
Matthew Griffith <mdg.griffith@gmail.com>
2025-09-12 08:51:15 -0400

    Listen for the real websocket messages via hot.js

--- ext-generate/Modify/Inject/hot.js
+++ ext-generate/Modify/Inject/hot.js
@@ -1,30 +1,64 @@
-; (function () {
-    try {
-        var wsUrl = "{{ ELM_DEV_SERVER_WS_URL }}";
-        var socket;
-        function connect() {
-            try {
-                socket = new WebSocket(wsUrl);
-                socket.onopen = function () { try { console.info('[elm-dev][hot] connected'); } catch (_) { } };
-                socket.onmessage = function (ev) {
-                    var msg = null;
-                    try { msg = JSON.parse(ev.data); } catch (_) { return; }
-                    try { window.dispatchEvent(new CustomEvent('elm-dev:hot-message', { detail: msg })); } catch (_) { }
-                    // If runtime provides hot apply hook, use it; else reload on success messages.
-                    try {
-                        if (window.Elm && window.Elm.Hot && typeof window.Elm.Hot.apply === 'function') {
-                            window.Elm.Hot.apply(msg);
-                        } else {
-                            if (msg && (msg.type === 'hot-apply' || msg.type === 'compile-success')) { location.reload(); }
-                        }
-                    } catch (_e) { /* last resort */ try { location.reload(); } catch (_) { } }
-                };
-                socket.onclose = function () { setTimeout(connect, 1000); };
-                socket.onerror = function () { try { socket.close(); } catch (_) { } };
-            } catch (_err) { setTimeout(connect, 2000); }
+
+
+(function () {
+    var wsUrl = "{{ ELM_DEV_SERVER_WS_URL }}";
+    var socket;
+    function applyCode(code) {
+        console.info('[elm-dev][hot] applying compiled js (length:', code && code.length, ')');
+        var f = new Function(code);
+        var newScope = {};
+        f.call(newScope);
+        if (window.Elm && window.Elm.hot && typeof window.Elm.hot.reload === 'function') {
+            console.info('[elm-dev][hot] calling Elm.hot.reload');
+            window.Elm.hot.reload(newScope);
+        } else {
+            console.info('[elm-dev][hot] Elm.hot.reload missing, full reload');
+            location.reload();
         }
-        connect();
-    } catch (_outer) { /* ignore */ }
+    }
+
+    function connect() {
+        socket = new WebSocket(wsUrl);
+        socket.onopen = function () { console.info('[elm-dev][hot] connected'); };
+        socket.onmessage = function (ev) {
+            try {
+                var raw = ev && ev.data;
+                console.info('[elm-dev][hot] raw message:', raw);
+                var msg = JSON.parse(raw);
+                console.info('[elm-dev][hot] parsed message:', msg);
+
+                // window.dispatchEvent(new CustomEvent('elm-dev:hot-message', { detail: msg }));
+
+                // New DevWS format: { msg: "Compiled" | "CompilationError", details: ... }
+                if (msg && typeof msg.msg === 'string') {
+                    if (msg.msg === 'Compiled') {
+                        if (msg.details && typeof msg.details === 'string' && msg.details.length > 0) {
+                            console.info('[elm-dev][hot] Compiled message with inline code; applying');
+                            applyCode(msg.details);
+                        }
+                    } else if (msg.msg === 'CompilationError') {
+                        console.info('[elm-dev][hot] compilation error received:', msg.details);
+                        // Do not reload on error; keep current app running
+                    }
+                    return;
+                }
+
+                // Legacy format support
+                var shouldApply = msg && (msg.type === 'hot-apply' || msg.type === 'compile-success');
+                if (!shouldApply) { return; }
+                fetchAndApply();
+            } catch (err) {
+                console.info('[elm-dev][hot] onmessage error', err);
+            }
+        };
+        socket.onclose = function () {
+            console.info('[elm-dev][hot] disconnected, reconnecting');
+            setTimeout(connect, 1000);
+        };
+        socket.onerror = function (err) {
+            console.info('[elm-dev][hot] error, reconnecting', err);
+            socket.close();
+        };
+    }
+    connect();
 })();
-
-
