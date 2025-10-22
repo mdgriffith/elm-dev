36efd28a8bcd7606fdbf2ebbb2ee31b50eb4ca11
Matthew Griffith <mdg.griffith@gmail.com>
2025-10-22 07:08:36 -0400

    Make the effect.ts compile nicely

--- ext-generate/generator/app/templates/toJs/js/effects.ts
+++ ext-generate/generator/app/templates/toJs/js/effects.ts
@@ -1,14 +1,25 @@
 /// <reference types="vite/client" />
 
+type EffectModule = { connect: (app: any, data: any) => void };
+
+function isEffectModule(value: unknown): value is EffectModule {
+  return (
+    typeof value === 'object' &&
+    value !== null &&
+    'connect' in value &&
+    typeof (value as any).connect === 'function'
+  );
+}
+
 // This imports all effect modules in the effect directory
 // and calls connect on them.
 export function connect(app: any, data: any) {
   const modules = import.meta.glob("./effect/*.ts", { eager: true });
-  for (const module of Object.values(modules)) {
-    if ("connect" in module && module.connect && typeof module.connect === 'function') {
-      module.connect(app, data);
+  for (const [path, mod] of Object.entries(modules)) {
+    if (isEffectModule(mod)) {
+      mod.connect(app, data);
     } else {
-      console.log(`${module} does not have a connect function`);
+      console.warn(`${path} does not export a 'connect' function`);
     }
   }
 }
