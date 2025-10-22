b248ef229db541742c73f7fdc41e997dff2014b8
Matthew Griffith <mdg.griffith@gmail.com>
2025-10-22 06:53:41 -0400

    Have elm-dev init use the published elm dev vite plugin

--- ext-generate/generator/app/templates/toRoot/vite.config.js
+++ ext-generate/generator/app/templates/toRoot/vite.config.js
@@ -1,26 +1,19 @@
 import { defineConfig } from "vite";
-import elmDev from "./viteElm";
+import elmDev from "elm-dev/vite";
 
-
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const isDev = mode == "development";
   return {
-    clearScreen: false,
-    server: {
-      strictPort: true,
-      open: true,
-    },
-
     build: {
       minify: "esbuild",
       outDir: "dist",
     },
-
     plugins: [
       elmDev({
         debug: isDev,
         optimize: !isDev,
       }),
     ],
-  };
+  }
 });
